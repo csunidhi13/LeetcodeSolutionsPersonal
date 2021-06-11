640f9da590fd10add4314e3fc3ad4677bdd693c9
sunidhichaudhary <sunidhichaudhary77@gmail.com>
2021-06-11 15:21:01 +0530

    Update 226. Invert Binary Tree.cpp

--- 226. Invert Binary Tree.cpp
+++ 226. Invert Binary Tree.cpp
@@ -15,3 +15,43 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        //using stack
+        if(!root) return NULL;
+        stack<TreeNode*> s{{root}};
+        while(!s.empty()){
+            TreeNode* node=s.top();
+            s.pop();
+            if(node){
+                s.push(node->left);
+                s.push(node->right);
+                swap(node->left,node->right);
+            }
+        }
+        return root;
+        
+//         Top Down
+//         if(!root) return NULL;
+//         TreeNode *r=root->right;
+//         TreeNode* l=root->left;
+//         root->left=r;
+//         root->right=l;
+//         invertTree(root->left);
+//         invertTree(root->right);
+//         return root;
+        
+//         Bottom Up
+//         if(!root){
+//             return NULL;
+            
+//         }
+//         TreeNode* temp=root->left;
+//         root->left=invertTree(root->right);
+//         root->right=invertTree(temp);
+//         return root;
+    }
+};
